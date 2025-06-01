1e459c28c7f58e6848bb981b9c7f6f03306a8fa7
willianmv <willianvmarques300@gmail.com>
2025-06-01 16:37:46 -0300

    feat(openAPI) configura Springdoc com autenticação JWT

---
+++ app/src/main/java/challenge/forumhub/app/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package challenge.forumhub.app.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+@OpenAPIDefinition(
+        info = @Info(title = "ForumHub REST API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth"))
+
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
+public class SwaggerConfig {
+}
